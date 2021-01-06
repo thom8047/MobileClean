30823db5a14ec3814f17696f9cef782dce6a4bea
eshop-uncproject <eshop.uncproject@gmail.com>
2021-01-06 12:06:00 -0700

    Update

---
+++ Mobile/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public void getButtons()
+        {
+            /* Change in the Reader class to create a button with every read, the text of the button and the content or link of 
+            button and append to a list of buttons that gets returned. With this returned list we can put each button inside a 
+            scroll view and from there see if it'll open with the Open class I wrote in.*/
+        }
+        public MainPage()
+        {
+            //InitializeComponent();
+            Label label = new Label
+            {
+                Text = "Brighten The Brain",
+                TextColor = Color.White,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.Center,
+            };
+
+            Frame frame = new Frame
+            {
+                Content = label,
+                Padding = 24,
+                BackgroundColor = Color.Black,
+            };
+            var stack = new StackLayout();
+            var scroll = new ScrollView();
+            var scrollStack = new StackLayout();
+            scroll.Content = scrollStack;
+            stack.BackgroundColor = Color.FromHex("#424242");
+            Content = stack;
+            stack.Children.Add(frame);
+            stack.Children.Add(scroll);
+
+            // Let's get Buttons
+            var obj = new Reader();
+            var button_data = obj.CSV();
+
+            foreach (var piece in button_data)
+            {
+                var button = new Button
+                {
+                    // Physical look of buttons
+                    Text = piece.ButtonName,
+                    FontSize = 32,
+                    VerticalOptions = LayoutOptions.Center,
+                    CornerRadius = 0,
+                    BackgroundColor = Color.FromHex("#42D7D6"),
+                    TextColor = Color.White,
+                };
+                button.Clicked += async (s, e) =>
+                {
+                    await Browser.OpenAsync(piece.Link);
+                };
+                scrollStack.Children.Add(button);
+            }
+        }
+    }
+}
---
+++ Mobile/Reader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public class ButtonData
+    {
+        public string FormName { get; set; }
+        public string ButtonName { get; set; }
+        public string Link { get; set; }
+        public ButtonData ReturnButton(string form_name, string button_name, string link)
+        {
+            ButtonData button = new ButtonData
+            {
+                FormName = form_name,
+                ButtonName = button_name,
+                Link = link
+            };
+
+            return button;
+        }
+    }
+    public class Reader
+    {
+        public List<ButtonData> CSV()
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(Reader)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Mobile.DataBase.csv");
+
+            using (var reader = new StreamReader(stream))
+            {
+                var list_of_buttons = new List<ButtonData>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    ButtonData data = new ButtonData
+                    {
+                        FormName = values[0],
+                        ButtonName = values[1],
+                        Link = values[2]
+                    };
+
+                    list_of_buttons.Add(data);
+                }
+
+                return list_of_buttons;
+            }
+        }
+    }
+}
